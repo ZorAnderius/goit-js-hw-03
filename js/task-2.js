855879f452cd7a9a165f59cf57e789dcd85a0f11
ZorAnderius <gabriel.zorander@gmail.com>
2024-04-13 07:54:25 +0100

    add task1, task2 and task3

---
+++ js/task-2.js
@@ -0,0 +1,31 @@
+/**join 2 arrays into one with max length
+ *
+ * @param {Array} firstArray
+ * @param {Array} secondArray
+ * @param {number} maxLength
+ * @returns {Array}
+ */
+function makeArray(firstArray, secondArray, maxLength) {
+  const joinedArray = [];
+  if (!maxLength) {
+    return joinedArray;
+  }
+
+  if (firstArray.length >= maxLength) {
+    return joinedArray.concat(firstArray.slice(0, maxLength));
+  } else if (firstArray.length < maxLength) {
+    return joinedArray.concat(
+      firstArray,
+      secondArray.slice(0, maxLength - firstArray.length)
+    );
+  }
+}
+
+console.log('-------------------------------------------------------------');
+console.log('Task 2');
+console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0)); // []
---
+++ js/task-3.js
@@ -0,0 +1,23 @@
+/**array filtering if the numbers exceed a given value
+ *
+ * @param {Array} numbers
+ * @param {number} value
+ * @returns {Array}
+ */
+function filterArray(numbers, value) {
+  const filterArray = [];
+  for (const num of numbers) {
+    if (num > value) {
+      filterArray.push(num);
+    }
+  }
+  return filterArray;
+}
+
+console.log('-------------------------------------------------------------');
+console.log('Task 3');
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
